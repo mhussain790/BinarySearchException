180722d555cc1fced9a5672ec741ccf1ae8bba4e
Masud Hussain <mhussain790@gmail.com>
2021-04-25 23:59:18 -0700

    Second commit of the bin_except.py file to repository.

--- bin_except.py
+++ bin_except.py
@@ -1,25 +1,44 @@
 """
-project-4a
-Modify the binary search function from the exploration so that,
-instead of returning -1 when the target value is not in
-the list, raises a TargetNotFound exception
-
-(you'll need to define this exception class).
-
-Otherwise it should function normally.
-
-Name this function bin_except.
+Name: Masud Hussain
+Course: CS162
+Assignment: 4A
 """
 
 
-def linear_search(a_list, target):
+# Initialize the TargetNotFound class that initializes a new message as a parameter
+class TargetNotFound(Exception):
+    def __init__(self, new_message):
+        self.message = new_message
+
+    def __str__(self):
+        return self.message
+
+
+# Binary search function that returns the index of the target variable from the list
+def binary_search(a_list, target):
     """
-    Searches a_list for an occurrence of target
-    If found, returns the index of its position in the list
-    If not found, returns -1, indicating the target value isn't in the list
-    """
-    # enumerate lets you iterate through both the elements of the list and their indices
-    for index, el in enumerate(a_list):
-        if el == target:
-            return index
-    return -1
+  Searches a_list for an occurrence of target
+  If found, returns the index of its position in the list
+  If not found, returns -1, indicating the target value isn't in the list
+  """
+    first = 0
+    last = len(a_list) - 1
+
+    # Initialize a boolean that is set as False by default
+    found = False
+
+    # Run the loop while found is false
+    if found is False:
+        while first <= last:
+            middle = (first + last) // 2
+            if a_list[middle] == target:
+                print(middle)
+                found = True
+            if a_list[middle] > target:
+                last = middle - 1
+            else:
+                first = middle + 1
+
+    # Raises exception error if there is no occurrence of the target var in the list
+    if not found:
+        raise TargetNotFound("Target not found")
